9550bdbc32d24885dff5f25bf109b7b75b4b61f5
Sathyam Vellal <sathyam.vellal@gmail.com>
2020-08-10 14:53:13 -0700

    Some basic eleventy configuration and a hello world

--- eleventy-website/.eleventy.js
+++ eleventy-website/.eleventy.js
@@ -1,3 +1,19 @@
-module.exports = {
-    markdownTemplateEngine: "njk"
+module.exports = function(eleventyConfig) {
+    eleventyConfig.addShortcode("greet", function(entity) {
+        return entity + "!";
+    });
+
+    return {
+        pathPrefix: "/",
+        htmlTemplateEngine: "njk",
+        dataTemplateEngine: "njk",
+        markdownTemplateEngine: "njk",
+        dir: {
+            input: "source",
+            output: "_site",
+            includes: "_includes",
+            layouts: "_layouts",
+            data: "_data"
+        }
+    };
 }
---
+++ eleventy-website/source/index.md
@@ -0,0 +1,6 @@
+---
+layout: default
+title: Title
+---
+
+# Hello, {% greet "World" %}
